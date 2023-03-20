04a7da85a97181c83b53bd5e69c5c71e9b9543c0
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-03-20 19:06:12 +0530

    initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+    const footerStyle = {
+        color : 'green',
+        fontStyle : 'italic',
+        fontSize : 16,
+        alignItems : 'center',
+        backgroundColor : 'aqua',
+        
+    }
+    return (
+        <div style = {footerStyle}>
+            <br />
+            <em>Note app, Made by Devanshu Gupta</em>
+        </div>
+    )
+}
+
+export default Footer
